1f1d6e32e48c0c1440d35749b519ff4ddb0478d7
Serdar <Serdar@DESKTOP-TU48RE3>
2017-09-28 10:24:27 +0300

    PersonClass

---
+++ ExampleApp/src/main/java/test/Person.java
@@ -0,0 +1,21 @@
+package test;
+
+public class Person {
+	
+	private String name;
+
+	
+	public Person() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
