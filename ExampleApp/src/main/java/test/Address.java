f0fe2329ffe0d365c037b33c8b8bc751d4ee9f69
Serdar <Serdar@DESKTOP-TU48RE3>
2017-09-28 10:44:45 +0300

    Added New Attribute in Address

--- ExampleApp/src/main/java/test/Address.java
+++ ExampleApp/src/main/java/test/Address.java
@@ -3,8 +3,16 @@ package test;
 public class Address {
 
 	private String street;
-
+	private int no;
 	
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
 	public Address() {
 		super();
 		// TODO Auto-generated constructor stub
