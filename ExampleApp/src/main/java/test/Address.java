3781b4f226ef855d59fed853c78c249c81c93424
Serdar <Serdar@DESKTOP-TU48RE3>
2017-09-28 10:38:44 +0300

    Adress Class Added !

---
+++ ExampleApp/src/main/java/test/Address.java
@@ -0,0 +1,20 @@
+package test;
+
+public class Address {
+
+	private String street;
+
+	
+	public Address() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+}
